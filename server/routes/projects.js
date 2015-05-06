1c70d560788239c28c0b1cd6229666cb598205f5
Scott Mackenzie <scott.s.mackenzie@gmail.com>
2015-05-07 02:22:24 +1000

    Project API - delete and create complete

---
+++ server/routes/projects.js
@@ -0,0 +1,125 @@
+/**
+ * Created by scottmackenzie on 7/05/15.
+ */
+
+var pg = require('pg'),
+    path = require('path'),
+    connectionString = require(path.join(__dirname, '../', '../', 'config'));
+
+
+var projects = {
+
+    getAll: function(req, res) {
+        pg.connect(connectionString, function(err, client, done) {
+            if(err) {
+                console.error(err.stack);
+                return res.status(500).send(err);
+            }
+            client.query("SELECT * FROM project", function(err, result) {
+                done();
+                if(err) {
+                    console.error(err.stack);
+                    return res.status(500).send(err);
+                }
+
+                client.end();
+                return res.json(result.rows);
+            });
+        })
+    },
+
+    getOne: function(req, res) {
+        pg.connect(connectionString, function(err, client, done) {
+            if(err) {
+                console.error(err.stack);
+                return res.status(500).send(err);
+            }
+            client.query("SELECT * FROM project WHERE project_name = $1", [req.body.projectName], function(err, result) {
+                done();
+                if(err) {
+                    console.error(err.stack);
+                    return res.status(500).send(err);
+                }
+                client.end();
+                return res.json(result.rows);
+            });
+        })
+    },
+
+    create: function(req, res) {
+        console.log('CREATE: ', req.body);
+        pg.connect(connectionString, function(err, client, done) {
+            if(err) {
+                console.error(err.stack);
+                return res.status(500).send(err);
+            }
+            if(req.body.projectName == '') {
+                return res.status(500).send(new Error('project name required'));
+            }
+            client.query("INSERT INTO project VALUES($1, $2, $3, $4, $5, $6, $7, $8)",
+                [req.body.projectName, req.body.description, req.body.budget, req.body.duration,
+                    req.body.startDate, req.body.estimatedEndDate, req.body.progress, req.body.projectManager], function(err, result) {
+                    done();
+                    if(err) {
+                        console.error(err.stack);
+                        return res.status(500).send(err);
+                    }
+                    client.end();
+                    return res.json(result.rows);
+                });
+        });
+    },
+
+    /* update, every field except for primary key must be updated */
+
+    update: function(req, res) {
+        console.log('UPDATE: ', req.body);
+        pg.connect(connectionString, function(err, client, done) {
+            if(err) {
+                console.error(err.stack);
+                return res.status(500).send(err);
+            }
+            if(req.body.projectName == '') {
+                return res.status(500).send(new Error('project name required'));
+            }
+            client.query("UPDATE project SET description=($2), budget=($3), duration=($4), " +
+                "start_date=($5), estimated_end_date=($6), progress=($7), project_manager=($8) WHERE project_name = $1",
+                [req.body.projectName, req.body.description, req.body.budget, req.body.duration,
+                    req.body.startDate, req.body.estimatedEndDate, req.body.progress, req.body.projectManager], function(err, result) {
+                    done();
+                    if(err) {
+                        console.error(err.stack);
+                        return res.status(500).send(err);
+                    }
+                    client.end();
+                    return res.json(result.rows);
+                });
+        });
+    },
+
+    delete: function(req, res) {
+        console.log('DELETE: ', req.body);
+        pg.connect(connectionString, function(err, client, done) {
+            if(err) {
+                console.error(err.stack);
+                return res.status(500).send(err);
+            }
+            if(req.body.projectName == '') {
+                return res.status(500).send(new Error('project name required'));
+            }
+            client.query("DELETE FROM project WHERE project_name = $1",
+                [req.body.email], function(err, result) {
+                    done();
+                    if(err) {
+                        console.error(err.stack);
+                        return res.status(500).send(err);
+                    }
+                    client.end();
+                    return res.json(result.rows);
+                });
+        });
+    }
+};
+
+module.exports = projects;
+
--- server/test/test.js
+++ server/test/test.js
@@ -135,3 +135,137 @@ describe('Employee', function(){
 
 
 })
+
+/**
+ * Testing Project API
+ */
+
+describe('Project', function(){
+    var server = 'http://localhost:3000';
+
+    var token = null;
+    var adminUser = {
+        email : 'admin@admin',
+        password : 'admin'
+    }
+    var testProject = {
+        projectName : 'Project 1',
+        description : 'Description of project',
+        budget : '500000',
+        duration : '365 days',
+        startDate : "01/01/2015",
+        estimatedEndDate : "02/02/2016",
+        progress : 0,
+        projectManager : "admin@admin"
+    };
+
+    before(function(done) {
+        superagent
+            .post(server + '/login')
+            .send({
+                email : adminUser.email,
+                password : adminUser.password
+            })
+            .end(function(err, res) {
+                if(err) console.log(err);
+                token = res.body.token; // Keep the token for authenticating test API calls
+                done();
+            });
+    })
+
+    it('should add project without error', function(done) {
+        superagent
+            .post(server + '/api/auth/admin/project/')
+            .set('X-Access-Token', token)
+            .set('X-Key', 'admin@admin')
+            .send({
+                projectName : testProject.projectName,
+                description : testProject.description,
+                budget : testProject.budget,
+                duration : testProject.duration,
+                startDate : testProject.startDate,
+                estimatedEndDate : testProject.estimatedEndDate,
+                progress : testProject.progress,
+                projectManager : testProject.projectManager
+            })
+            .end(function(err, res) {
+                expect(err).to.eql(null);
+                expect(res.status).to.eql(200);
+                done();
+            });
+    });
+
+    /*it('should retrieve the employee without error', function(done) {
+        superagent
+            .get(server + '/api/auth/admin/user/' + testUser.email)
+            .set('X-Access-Token', token)
+            .set('X-Key', 'admin@admin')
+            .send({
+                email: testUser.email
+            })
+            .end(function(err, res) {
+                expect(err).to.eql(null);
+                expect(res.status).to.eql(200);
+                expect(res.body[0].email).to.eql(testUser.email);
+                done();
+            });
+    })
+
+    it('should retrieve all employees without error', function(done) {
+        superagent
+            .get(server + '/api/auth/admin/users/')
+            .set('X-Access-Token', token)
+            .set('X-Key', 'admin@admin')
+            .send({
+                email: testUser.email
+            })
+            .end(function(err, res) {
+                expect(err).to.eql(null);
+                expect(res.status).to.eql(200);
+                done();
+            });
+    })
+
+    it('should update employee without error', function(done) {
+        superagent
+            .put(server + '/api/auth/admin/user/' + testUser.email)
+            .set('X-Access-Token', token)
+            .set('X-Key', 'admin@admin')
+            .send({
+                email: testUser.email,
+                firstname : 'newfirstname',
+                lastname : 'newlastname',
+                password : testUser.password,
+                phone : '0987654321',
+                role : testUser.role,
+                performanceIndex : 30,
+                previousRoles : ['tester', 'documenter']
+            })
+            .end(function(err, res) {
+                expect(err).to.eql(null);
+                expect(res.status).to.eql(200);
+                done();
+            });
+    });
+
+*/
+    it('should delete project without error', function(done) {
+        //console.log('TOKEN', token);
+        superagent
+            .del(server + '/api/auth/admin/project/')
+            .set('X-Access-Token', token)
+            .set('X-Key', 'admin@admin')
+            .set('Accept', 'application/json')
+            .send({
+                projectName: testProject.projectName
+            })
+            .end(function(err, res) {
+                expect(err).to.eql(null);
+                expect(res.status).to.eql(200);
+                done();
+            });
+    });
+
+
+
+})
