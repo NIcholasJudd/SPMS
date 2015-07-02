426a24b9164d2a322d955530d2a00cf0fb26e8b0
Nicholas Judd <noj999@uowmail.edu.au>
2015-07-02 15:00:02 +1000

    links back into header pm-dash link works

---
+++ client/js/header.controller.js
@@ -0,0 +1,14 @@
+/**
+ * Created by nicholasjudd on 2/07/15.
+ */
+myApp.controller("HeaderCtrl", ['$scope', '$window', '$location', 'UserAuthFactory',
+    function($scope, $window, $location, UserAuthFactory) {
+        $scope.isActive = function(route) {
+            return route === $location.path();
+        }
+        $scope.userType = $window.sessionStorage.userRole;
+        $scope.logout = function () {
+            UserAuthFactory.logout();
+        }
+    }
+]);
