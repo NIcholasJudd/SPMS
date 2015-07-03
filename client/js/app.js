070a6446c926dab4e7dee601de59c6fb50619e47
Nicholas Judd <noj999@uowmail.edu.au>
2015-07-03 15:52:55 +1000

    Modify user form layout correct partial framework written Still needs work

--- client/js/app.js
+++ client/js/app.js
@@ -1,261 +1,270 @@
 var myApp = angular.module('ngclient', ['ui.router', 'ui.bootstrap', 'ui.slider', 'ui.bootstrap.typeahead', 'ui.bootstrap.tabs', 'mj.scrollingTabs', 'angularModalService']);
 
-myApp.config(function($stateProvider, $urlRouterProvider, $httpProvider, $locationProvider) {
-  //add Token Interceptor
-  $httpProvider.interceptors.push('TokenInterceptor');
+myApp.config(function ($stateProvider, $urlRouterProvider, $httpProvider, $locationProvider) {
+    //add Token Interceptor
+    $httpProvider.interceptors.push('TokenInterceptor');
 
     // use the HTML5 History API
     //$locationProvider.html5Mode(true).hashPrefix('!');
 
-  $urlRouterProvider.otherwise('/');
+    $urlRouterProvider.otherwise('/');
 
-  $stateProvider
-      .state('login', {
-          url: "/login",
-          views: {
-              'container@' : {
-                  templateUrl: "/views/login.html",
-                  controller: "LoginCtrl"
-              }
-          }
+    $stateProvider
+        .state('login', {
+            url: "/login",
+            views: {
+                'container@': {
+                    templateUrl: "/views/login.html",
+                    controller: "LoginCtrl"
+                }
+            }
 
-      })
-      .state('app', {
-          url: "",
-          abstract: true,
-          views: {
-              'header': {
-                  templateUrl: '/views/header.html',
-                  controller: 'HeaderCtrl'
-              }
-          }
-      })
-      .state('app.home', {
-          url: "/",
-          views: {
-              'container@' : {
-                  templateUrl: "views/home.html"
-              }
-          }
-      })
-      .state('app.dashboard', {
-          url: "/dashboard",
-          abstract: true,
-          views: {
-              'container@' : {
-                  template: "<ui-view/>"// Note: abstract still needs a ui-view for its children to populate.
-              }
-          }
-      })
-      .state('app.dashboard.projectManager', {
-          url: "/project-manager",
-          views: {
-              'container@' : {
-                  templateUrl: "views/dashboards/projectManager/container.html",
-                  controller: "PMContainerCtrl"
-              },
-              'projecttracking@app.dashboard.projectManager' : {
-                  templateUrl: "views/dashboards/projectManager/project-tracking.html"
-              }/*,
-              'tasks@app.dashboard.projectManager' : {
-                  templateUrl: "views/dashboards/projectManager/tasks.html"
-              },
-              'projectstatistics@app.dashboard.projectManager' : {
-                  templateUrl: "views/dashboards/projectManager/project-statistics.html"
-              },
-              'projectprogression@app.dashboard.projectManager' : {
-                  templateUrl: "views/dashboards/projectManager/project-progression.html"
-              }*/
-          }
-      })
-      .state('app.userCreate', {
-          url: "/user/create",
-          views: {
-              'container@' : {
-                  templateUrl: "views/user/create.html"
-              }
-          }
-      });
+        })
+        .state('app', {
+            url: "",
+            abstract: true,
+            views: {
+                'header': {
+                    templateUrl: '/views/header.html',
+                    controller: 'HeaderCtrl'
+                }
+            }
+        })
+        .state('app.home', {
+            url: "/",
+            views: {
+                'container@': {
+                    templateUrl: "views/home.html"
+                }
+            }
+        })
+        .state('app.dashboard', {
+            url: "/dashboard",
+            abstract: true,
+            views: {
+                'container@': {
+                    template: "<ui-view/>"// Note: abstract still needs a ui-view for its children to populate.
+                }
+            }
+        })
+        .state('app.dashboard.projectManager', {
+            url: "/project-manager",
+            views: {
+                'container@': {
+                    templateUrl: "views/dashboards/projectManager/container.html",
+                    controller: "PMContainerCtrl"
+                },
+                'projecttracking@app.dashboard.projectManager': {
+                    templateUrl: "views/dashboards/projectManager/project-tracking.html"
+                }/*,
+                 'tasks@app.dashboard.projectManager' : {
+                 templateUrl: "views/dashboards/projectManager/tasks.html"
+                 },
+                 'projectstatistics@app.dashboard.projectManager' : {
+                 templateUrl: "views/dashboards/projectManager/project-statistics.html"
+                 },
+                 'projectprogression@app.dashboard.projectManager' : {
+                 templateUrl: "views/dashboards/projectManager/project-progression.html"
+                 }*/
+            }
+        })
+        .state('app.userCreate', {
+            url: "/user/create",
+            views: {
+                'container@': {
+                    templateUrl: "views/user/create.html"
+                }
+            }
+        })
+        .state('app.userModify', {
+            url: "/user/modify",
+            views: {
+                'container@': {
+                    templateUrl: "views/user/modify.html"
+                },
+                'modifyForm@app.userModify': {
+                    templateUrl: "views/user/modifyForm.html"
+                }
+            }
+            });
 
-
-
-  /*$routeProvider
-      .when('/login', {
-        templateUrl: 'partials/login.html',
-        controller: 'LoginCtrl',
-        access: {
-          requiredLogin: false
-        }
-      }).when('/', {
-        templateUrl: 'partials/home.html',
-        controller: 'LoginCtrl',
-        access: {
-          requiredLogin: true
-        }
-      }).when('/project/create', {
-        templateUrl: 'partials/project/project.create.html',
-        controller: 'ProjectCtrl',
-        access: {
-          requiredLogin: true,
-            adminOnly: true
-        }
-      }).when('/project/modify', {
-          templateUrl: 'partials/project/project.modify.html',
-          controller: 'ProjectModCtrl',
-          access: {
-              requiredLogin: true,
-              adminOnly: false
-          },
-          resolve : {
-              currentProject : function(ProjectFactory, $window) {
-                  return ProjectFactory.getProject($window.sessionStorage.projectName);
-              }
-          }
-      }).when('/project/archive', {
-          templateUrl: 'partials/project/project.archive.html',
-          controller: 'ProjectArchiveCtrl',
-          access: {
-              requiredLogin: true,
-              adminOnly : true
-          },
-          resolve: {
-              ActiveProjects : function(ProjectFactory) {
-                  return ProjectFactory.getProjects();
-              },
-              ArchivedProjects : function(ProjectFactory) {
-                  return ProjectFactory.getArchivedProjects();
-              }
-          }
-      }).when('/project/taskCreate', {
-        templateUrl: 'partials/project/task.create.html',
-        controller: 'TaskCreateCtrl',
-        access: {
-          requiredLogin: true,
-            adminOnly : false
-        }
-      }).when('/project/taskModify', {
-          templateUrl: 'partials/project/task.modify.html',
-          controller: 'TaskModCtrl',
-          access: {
-              requiredLogin: true,
-              adminOnly : false
-          }
-      }).when('/project/taskDetails', {
-          templateUrl: 'partials/project/task.detail.html',
-          controller: 'TaskDetailCtrl',
-          access: {
-              requiredLogin: true,
-              adminOnly : false
-          }
-      }).when('/effort/functionPoints', {
-        templateUrl: 'partials/effort/functionPoints.html',
-        controller: 'EffortFunctionPointCtrl',
-        access: {
-              requiredLogin: true,
-              adminOnly: false
-          },
-          resolve : {
-              FunctionPointData : function(ProjectFactory, $window) {
-                  return ProjectFactory.getFunctionPointData($window.sessionStorage.projectName);
-              }
-          }
-      }).when('/effort/effortEstimation', {
-        templateUrl: 'partials/effort/effort.estimation.html',
-        controller: 'EffortCocomoCtrl',
-        access: {
-              requiredLogin: true,
-              adminOnly: false
-          },
-          resolve : {
-              CocomoScores : function(ProjectFactory, $window) {
-                  return ProjectFactory.getCocomoScores($window.sessionStorage.projectName);
-              }
-          }
-      }).when('/project/ganttChart', {
-        templateUrl: 'partials/project/ganttChart.html',
-        controller: 'GanttChartCtrl',
-        access: {
-              requiredLogin: true,
-              adminOnly: false
-          }
-      }).when('/project/apn', {
-          templateUrl: 'partials/project/apn.html',
-          controller: 'APNCtrl',
-          access: {
-              requiredLogin: true,
-              adminOnly : false
-          }
-      }).when('/dashboard/pmDashboard', {
-        templateUrl: 'partials/dashboard/project.manager.dashboard.html',
-        controller: 'ProjectDashboardCtrl',
-        access: {
-          requiredLogin: true,
-            adminOnly : false
-        },
-          resolve : {
-              assignedProjects : function(ProjectFactory, $window) {
-                  if($window.sessionStorage.userRole === 'administrator')
-                      return ProjectFactory.getProjects();
-                  else {
-                      console.log('team member');
-                      return ProjectFactory.getPMProjects($window.sessionStorage.user);
-                  }
-              }
-          }
-      }).when('/dashboard/tmDashboard', {
-        templateUrl: 'partials/dashboard/team.member.dashboard.html',
-        controller: 'TaskDashCtrl',
-        access: {
-          requiredLogin: true,
-            adminOnly : false
-        }
-      }).when('/modal', {
-          templateUrl: 'partials/modal.html',
-          controller: 'HomeCtrl',
-          access: {
-              requiredLogin: true,
-              adminOnly : false
-          }
-      }).when('/user/create', {
-        templateUrl: 'partials/user/user.create.html',
-        controller: 'UserCtrl',
-        access: {
-          requiredLogin: true,
-            adminOnly : true
-        }
-      }).when('/error', {
-          templateUrl: 'partials/error.html'
-      }).otherwise({
-        redirectTo: '/login'
-      });*/
+    /*$routeProvider
+     .when('/login', {
+     templateUrl: 'partials/login.html',
+     controller: 'LoginCtrl',
+     access: {
+     requiredLogin: false
+     }
+     }).when('/', {
+     templateUrl: 'partials/home.html',
+     controller: 'LoginCtrl',
+     access: {
+     requiredLogin: true
+     }
+     }).when('/project/create', {
+     templateUrl: 'partials/project/project.create.html',
+     controller: 'ProjectCtrl',
+     access: {
+     requiredLogin: true,
+     adminOnly: true
+     }
+     }).when('/project/modify', {
+     templateUrl: 'partials/project/project.modify.html',
+     controller: 'ProjectModCtrl',
+     access: {
+     requiredLogin: true,
+     adminOnly: false
+     },
+     resolve : {
+     currentProject : function(ProjectFactory, $window) {
+     return ProjectFactory.getProject($window.sessionStorage.projectName);
+     }
+     }
+     }).when('/project/archive', {
+     templateUrl: 'partials/project/project.archive.html',
+     controller: 'ProjectArchiveCtrl',
+     access: {
+     requiredLogin: true,
+     adminOnly : true
+     },
+     resolve: {
+     ActiveProjects : function(ProjectFactory) {
+     return ProjectFactory.getProjects();
+     },
+     ArchivedProjects : function(ProjectFactory) {
+     return ProjectFactory.getArchivedProjects();
+     }
+     }
+     }).when('/project/taskCreate', {
+     templateUrl: 'partials/project/task.create.html',
+     controller: 'TaskCreateCtrl',
+     access: {
+     requiredLogin: true,
+     adminOnly : false
+     }
+     }).when('/project/taskModify', {
+     templateUrl: 'partials/project/task.modify.html',
+     controller: 'TaskModCtrl',
+     access: {
+     requiredLogin: true,
+     adminOnly : false
+     }
+     }).when('/project/taskDetails', {
+     templateUrl: 'partials/project/task.detail.html',
+     controller: 'TaskDetailCtrl',
+     access: {
+     requiredLogin: true,
+     adminOnly : false
+     }
+     }).when('/effort/functionPoints', {
+     templateUrl: 'partials/effort/functionPoints.html',
+     controller: 'EffortFunctionPointCtrl',
+     access: {
+     requiredLogin: true,
+     adminOnly: false
+     },
+     resolve : {
+     FunctionPointData : function(ProjectFactory, $window) {
+     return ProjectFactory.getFunctionPointData($window.sessionStorage.projectName);
+     }
+     }
+     }).when('/effort/effortEstimation', {
+     templateUrl: 'partials/effort/effort.estimation.html',
+     controller: 'EffortCocomoCtrl',
+     access: {
+     requiredLogin: true,
+     adminOnly: false
+     },
+     resolve : {
+     CocomoScores : function(ProjectFactory, $window) {
+     return ProjectFactory.getCocomoScores($window.sessionStorage.projectName);
+     }
+     }
+     }).when('/project/ganttChart', {
+     templateUrl: 'partials/project/ganttChart.html',
+     controller: 'GanttChartCtrl',
+     access: {
+     requiredLogin: true,
+     adminOnly: false
+     }
+     }).when('/project/apn', {
+     templateUrl: 'partials/project/apn.html',
+     controller: 'APNCtrl',
+     access: {
+     requiredLogin: true,
+     adminOnly : false
+     }
+     }).when('/dashboard/pmDashboard', {
+     templateUrl: 'partials/dashboard/project.manager.dashboard.html',
+     controller: 'ProjectDashboardCtrl',
+     access: {
+     requiredLogin: true,
+     adminOnly : false
+     },
+     resolve : {
+     assignedProjects : function(ProjectFactory, $window) {
+     if($window.sessionStorage.userRole === 'administrator')
+     return ProjectFactory.getProjects();
+     else {
+     console.log('team member');
+     return ProjectFactory.getPMProjects($window.sessionStorage.user);
+     }
+     }
+     }
+     }).when('/dashboard/tmDashboard', {
+     templateUrl: 'partials/dashboard/team.member.dashboard.html',
+     controller: 'TaskDashCtrl',
+     access: {
+     requiredLogin: true,
+     adminOnly : false
+     }
+     }).when('/modal', {
+     templateUrl: 'partials/modal.html',
+     controller: 'HomeCtrl',
+     access: {
+     requiredLogin: true,
+     adminOnly : false
+     }
+     }).when('/user/create', {
+     templateUrl: 'partials/user/user.create.html',
+     controller: 'UserCtrl',
+     access: {
+     requiredLogin: true,
+     adminOnly : true
+     }
+     }).when('/error', {
+     templateUrl: 'partials/error.html'
+     }).otherwise({
+     redirectTo: '/login'
+     });*/
 });
-myApp.run(function($rootScope, $window, $location, AuthenticationFactory) {
+myApp.run(function ($rootScope, $window, $location, AuthenticationFactory) {
 // when the page refreshes, check if the user is already logged in
-  AuthenticationFactory.check();
-  $rootScope.$on("$routeChangeStart", function(event, nextRoute, currentRoute) {
-    if ((nextRoute.access && nextRoute.access.requiredLogin) && !AuthenticationFactory.isLogged) {
-      $state.go("login");
-    } else {
-        /* if user doesn't have admin and tries to access admin page, direct to error message page*/
-        //this line below fixed the 'not authorised on refresh' error... <-- might need to check for vulnerabilities caused by it
-        if (!AuthenticationFactory.userRole) AuthenticationFactory.userRole = $window.sessionStorage.userRole;
-        if (!AuthenticationFactory.user) AuthenticationFactory.user = $window.sessionStorage.user;
-        if((nextRoute.access && nextRoute.access.adminOnly) && AuthenticationFactory.userRole != 'administrator') {
-            $location.path("/error").replace();
+    AuthenticationFactory.check();
+    $rootScope.$on("$routeChangeStart", function (event, nextRoute, currentRoute) {
+        if ((nextRoute.access && nextRoute.access.requiredLogin) && !AuthenticationFactory.isLogged) {
+            $state.go("login");
         } else {
-            // check if user object exists else fetch it. This is incase of a page refresh
-            if (!AuthenticationFactory.user) AuthenticationFactory.user = $window.sessionStorage.user;
+            /* if user doesn't have admin and tries to access admin page, direct to error message page*/
+            //this line below fixed the 'not authorised on refresh' error... <-- might need to check for vulnerabilities caused by it
             if (!AuthenticationFactory.userRole) AuthenticationFactory.userRole = $window.sessionStorage.userRole;
-            console.log('myapp.run: ', AuthenticationFactory.userRole);
+            if (!AuthenticationFactory.user) AuthenticationFactory.user = $window.sessionStorage.user;
+            if ((nextRoute.access && nextRoute.access.adminOnly) && AuthenticationFactory.userRole != 'administrator') {
+                $location.path("/error").replace();
+            } else {
+                // check if user object exists else fetch it. This is incase of a page refresh
+                if (!AuthenticationFactory.user) AuthenticationFactory.user = $window.sessionStorage.user;
+                if (!AuthenticationFactory.userRole) AuthenticationFactory.userRole = $window.sessionStorage.userRole;
+                console.log('myapp.run: ', AuthenticationFactory.userRole);
+            }
         }
-    }
-  });
-  $rootScope.$on('$routeChangeSuccess', function(event, nextRoute, currentRoute) {
-    $rootScope.showMenu = AuthenticationFactory.isLogged;
-    $rootScope.role = AuthenticationFactory.userRole;
+    });
+    $rootScope.$on('$routeChangeSuccess', function (event, nextRoute, currentRoute) {
+        $rootScope.showMenu = AuthenticationFactory.isLogged;
+        $rootScope.role = AuthenticationFactory.userRole;
 // if the user is already logged in, take him to the home page
-    if (AuthenticationFactory.isLogged == true && $location.path() == '/login') {
-      $state.go('/');
-    }
-  });
+        if (AuthenticationFactory.isLogged == true && $location.path() == '/login') {
+            $state.go('/');
+        }
+    });
 });
--- client/js/user/user.controller.js
+++ client/js/user/user.controller.js
@@ -1,141 +1,94 @@
 /**
- * Created by scottmackenzie on 5/05/2015.
+ * Created by Nicholas Judd 2/07/15
  */
-myApp.controller("UserCtrl", ['$scope', 'UserFactory',
-    function($scope, UserFactory) {
+//Create User Controllers
+myApp.controller("userCreate", ['$scope', 'UserFactory',
+    function ($scope, UserFactory) {
+        $scope.roles = [
+            {name: 'Administrator'},
+            {name: 'Team Member'}];
 
-        $scope.user = {
-            email : "",
-            firstName : "",
-            lastName : "",
-            password : "",
-            phone : "",
-            role : "",
-            performanceIndex : 0,
-            previousRoles : ['Developer', 'Tester']
-        };
-        $scope.roles = [{name: 'Administrator'}, {name: 'Team Member'}];
-        $scope.selected = undefined;
         $scope.skills = [
-            { title: 'C++'},
-            { title: 'Java'},
-            { title: 'MySQL'},
-            { title: 'HTML'},
-            { title: 'JavaScript'},
-            { title: 'AngularJS'},
-            { title: 'C'},
-            { title: 'Python'},
-            { title: 'C#'},
-            { title: 'Objective C'}
+            {title: 'C++'},
+            {title: 'Java'},
+            {title: 'MySQL'},
+            {title: 'HTML'},
+            {title: 'JavaScript'},
+            {title: 'AngularJS'},
+            {title: 'C'},
+            {title: 'Python'},
+            {title: 'C#'},
+            {title: 'Objective C'}
         ];
+        $scope.user = {
+            email: "",
+            firstName: "",
+            lastName: "",
+            password: "",
+            phone: "",
+            role: "",
+            performanceIndex: 0,
+            skills: [],
+            previousRoles: ['Developer', 'Tester']
+        };
 
         $scope.selectedRole = {name: 'Available Roles:'};
 
-        $scope.setRole= function(role){
-          $scope.selectedRole = role;
+
+        $scope.setRole = function (role) {
+            console.log("TEST");
+            $scope.selectedRole = role;
             $scope.user.role = role.name.toLowerCase();
         };
 
-        $scope.show = function() {
-            console.log($scope.user);
-        }
-        $scope.saveUser = function() {
+        $scope.submitUser = function () {
             var msg = "Form error: ";
             var ok = true;
-            if(!$scope.user.firstName) {
+            if (!$scope.user.firstName) {
                 ok = false;
                 msg += "first name missing\n";
             }
-            if(!$scope.user.lastName) {
+            if (!$scope.user.lastName) {
                 ok = false;
                 msg += "last name missing\n";
             }
-            if(!$scope.user.email) {
+            if (!$scope.user.email) {
                 ok = false;
                 msg += "email missing\n";
             }
-            if(!$scope.user.password) {
+            if (!$scope.user.password) {
                 ok = false;
                 msg += "password missing\n";
             }
-            if(!$scope.user.role) {
+            if (!$scope.user.role) {
                 ok = false;
                 msg += "role missing\n";
             }
-            if(ok === false) {
+            if (ok === false) {
                 alert(msg);
                 return;
             }
             console.log("USER: ", $scope.user);
-            UserFactory.createUser($scope.user).success(function(err, res) {
+            UserFactory.createUser($scope.user).success(function (err, res) {
                 alert($scope.user.email + ' successfully saved in database');
-            }).error(function(err, res) {
+            }).error(function (err, res) {
                 var msg = "Create user failed: " + err;
                 console.log(err);
                 alert(msg);
             })
         }
 
-        $scope.submitUser = function() {
-            console.log($scope.user);
-        }
-
-
-
-    }
-]);
-
-//Created by Nicholas Judd 2/7/15
-myApp.controller("userCreate", ['$scope', 'UserFactory',
-    function($scope, UserFactory) {
-        $scope.roles = [
-            {name: 'Administrator'},
-            {name: 'Team Member'}];
-
-        $scope.skills = [
-            { title: 'C++'},
-            { title: 'Java'},
-            { title: 'MySQL'},
-            { title: 'HTML'},
-            { title: 'JavaScript'},
-            { title: 'AngularJS'},
-            { title: 'C'},
-            { title: 'Python'},
-            { title: 'C#'},
-            { title: 'Objective C'}
-        ];
-        $scope.user = {
-            email : "",
-            firstName : "",
-            lastName : "",
-            password : "",
-            phone : "",
-            role : "",
-            performanceIndex : 0,
-            skills : [],
-            previousRoles : ['Developer', 'Tester']
-        };
-
-        $scope.selectedRole = {name: 'Available Roles:'};
-
-
-        $scope.setRole= function(role){
-            console.log("TEST");
-            $scope.selectedRole = role;
-            $scope.user.role = role.name.toLowerCase();
-        };
-
-        $scope.saveUser = function() {
+        $scope.saveUser = function () {
             $scope.user.skills = $('#userSkill').val();
             console.log($scope.user);
         }
     }
 ]);
 
-myApp.controller("passwordCheck", ['$scope',
-    function($scope) {
-
-        $('#pass').keyup(function(e) {
+myApp.controller("passwordCheck", [
+    //check password strength
+    function () {
+        $('#pass').keyup(function (e) {
             var strongRegex = new RegExp("^(?=.{8,})(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*\\W).*$", "g");
             var mediumRegex = new RegExp("^(?=.{7,})(((?=.*[A-Z])(?=.*[a-z]))|((?=.*[A-Z])(?=.*[0-9]))|((?=.*[a-z])(?=.*[0-9]))).*$", "g");
             var enoughRegex = new RegExp("(?=.{3,}).*", "g");
@@ -151,27 +104,78 @@ myApp.controller("passwordCheck", ['$scope',
                 $('#passstrength').className = 'error';
                 $('#passstrength').html('Weak!');
             }
+            validate();
             return true;
         });
-
-        $(document).ready(function() {
+        $(document).ready(function () {
             $("#pass2").keyup(validate);
         });
-
+        //checks passwords are the same
         function validate() {
             var password1 = $("#pass").val();
             var password2 = $("#pass2").val();
-
-
-
-            if(password1 == password2) {
+            if (password1 == password2) {
                 $("#validate-status").text("valid");
             }
             else {
                 $("#validate-status").text("invalid");
             }
-
         }
     }
-
 ]);
+
+//Modify Users Controllers
+myApp.controller("userModify", ['$scope','UserFactory',
+    function ($scope, UserFactory) {
+        $scope.user = [];
+        $scope.selectedNames = undefined;
+        $scope.activeUser = {};
+        UserFactory.getUsers().then(function (results) {
+            results.data.forEach(function (user) {
+                $scope.user.push({
+                    name: user.first_name + ' ' + user.last_name,
+                    firstName: user.first_name,
+                    lastName: user.last_name,
+                    email: user.email,
+                    phone: user.phone,
+                    performanceIndex: user.performance_index * 10,
+                    role: user.user_type,
+                    previousRoles: user.previous_roles,
+                    active: user.active
+                });
+            })
+        })
+        $scope.skills = [
+            {title: 'C++'},
+            {title: 'Java'},
+            {title: 'MySQL'},
+            {title: 'HTML'},
+            {title: 'JavaScript'},
+            {title: 'AngularJS'},
+            {title: 'C'},
+            {title: 'Python'},
+            {title: 'C#'},
+            {title: 'Objective C'}
+        ];
+        $scope.roles = [
+            {name: 'Administrator'},
+            {name: 'Team Member'}];
+        $scope.selectedRole;
+        $scope.setRole = function (role) {
+            console.log("TEST");
+            $scope.selectedRole = role;
+            $scope.activeUser.role = role.name.toLowerCase();
+        };
+
+        $scope.updateForm = false;
+        $scope.findUser = function (name) {
+            for (var i = 0; i < $scope.user.length; i++) {
+                if (name == $scope.user[i].name) {
+                    $scope.activeUser = $scope.user[i];
+                    $scope.selectedRole = {name:$scope.activeUser.role}
+                };
+            };
+            console.log($scope.activeUser);
+            $scope.updateForm = true;
+        };
+    }]);
